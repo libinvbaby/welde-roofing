40d620d271980b28170056cb43dc70116210db1e
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-02-14 12:46:59 +0530

    updated

--- sendemail.php
+++ sendemail.php
@@ -13,6 +13,7 @@ require 'phpmailer/src/SMTP.php';
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
+$city = $_POST['city'];
 $message = $_POST['message'];
 
 // Create a new instance of the PHPMailer class
@@ -22,27 +23,29 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
-$mail->Username = 'vblibin885@gmail.com';
+$mail->Username = 'Your mail';
 $mail->Password = 'xpcqyghkvbfqaiee';
 $mail->SMTPSecure = 'ssl';
 $mail->Port = 465;
 
 // Set the recipient, subject, and message
 $mail->setFrom($email, $name);
-$mail->addAddress('vblibin885@gmail.com');
-$mail->Subject = 'New Contact Form Submission from Solar Landing Page';
+$mail->addAddress('Your mail');
+$mail->Subject = 'New Contact Form Submission from Roofing Landing Page';
 $mail->Body = "
 <table style='border-collapse: collapse; width: 100%;'>
   <tr style='border: 1px solid black; background-color: #f2f2f2;'>
     <th style='padding: 8px; text-align: left; border: 1px solid black;'>Name</th>
     <th style='padding: 8px; text-align: left; border: 1px solid black;'>Email</th>
     <th style='padding: 8px; text-align: left; border: 1px solid black;'>Phone</th>
+    <th style='padding: 8px; text-align: left; border: 1px solid black;'>city</th>
     <th style='padding: 8px; text-align: left; border: 1px solid black;'>Message</th>
   </tr>
   <tr style='border: 1px solid black;'>
     <td style='padding: 8px; text-align: left; border: 1px solid black;'>$name</td>
     <td style='padding: 8px; text-align: left; border: 1px solid black;'>$email</td>
     <td style='padding: 8px; text-align: left; border: 1px solid black;'>$phone</td>
+    <td style='padding: 8px; text-align: left; border: 1px solid black;'>$city</td>
     <td style='padding: 8px; text-align: left; border: 1px solid black;'>$message</td>
   </tr>
 </table>";
